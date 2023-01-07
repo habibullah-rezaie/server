cf7434a5f8897c41f27a3b48ae2ebb7763b02f99
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-01-07 12:37:24 +0300

    Update Prisma schema

--- index.ts
+++ index.ts
@@ -15,7 +15,8 @@ const port = process.env.PORT || "8000";
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: /http:\/\/localhost:\d*/,
+
 		credentials: true,
 	})
 );
