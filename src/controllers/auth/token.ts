217268305a8d5d4eadeb717037a674d00e466018
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-01-07 12:39:39 +0300

    Check sessions on creating a token

--- src/controllers/auth/token.ts
+++ src/controllers/auth/token.ts
@@ -2,70 +2,58 @@ import { User } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import prisma from "../../../prisma/prisma";
+import { removeAuthCookies } from "../../util/auth";
+import { AccessTkPayload, RefreshTkPayload } from "./login";
 
 const createToken = async (req: Request, res: Response, next: NextFunction) => {
+	if (!(req as any).session) {
+		return removeAuthCookies(res)
+			.status(401)
+			.json({ message: "Session expired" });
+	}
+
 	if (!req.user) {
 		return res.status(500).json({ message: "InternalServerError" });
 	}
 
-	const reqUser = req.user as User;
+	// added an sid when athenticating with refreshToken
+	const reqUser = req.user as RefreshTkPayload;
 
-	if (reqUser.email && reqUser.role && process.env.JWT_REFRESH_SECRET) {
-		const user = await prisma.user.findUnique({
-			where: { email: reqUser.email },
-		});
+	console.log(req.user);
+	if (!reqUser.userId || !reqUser.role) {
+		console.error("Something has went wrong, email or role is in `req.user`");
+		res.status(500).json({ message: "InternalServerError" });
+	}
 
-		if (!user) return res.status(400).json({ message: "Bad Request" });
-
-		const { email, role, name } = user;
-
-		return jwt.sign(
-			{ email, role, name },
-			process.env.JWT_REFRESH_SECRET,
-			{
-				algorithm: "HS256",
-				expiresIn: "1d",
-			},
-
-			(err, refreshToken) => {
-				if (err || !process.env.JWT_ACCESS_SECRET)
-					return res.status(500).json({ message: "InternalServerError" });
-
-				return jwt.sign(
-					{ email, role, name },
-					process.env.JWT_ACCESS_SECRET,
-					{
-						algorithm: "HS256",
-						expiresIn: "1h",
-					},
-					(err, accessToken) => {
-						if (err)
-							return res.status(500).json({ message: "InternalServerError" });
-
-						return res
-							.cookie("refreshToken", refreshToken, {
-								expires: new Date(Date.now() + 86400 * 1000),
-								secure: true,
-								httpOnly: true,
-							})
-							.cookie("accessToken", accessToken, {
-								expires: new Date(Date.now() + 60 * 1000),
-								secure: true,
-								httpOnly: true,
-							})
-							.status(200)
-							.send();
-					}
-				);
-			}
-		);
-	} else if (!process.env.JWT_REFRESH_SECRET) {
-		console.error("No jwt refresh token secret in env");
+	if (!process.env.JWT_ACCESS_SECRET) {
+		console.error(new Error("No access token in environment variables"));
 		return res.status(500).json({ message: "InternalServerError" });
 	}
 
-	console.error("Something has went wrong, email or role is in `req.user`");
-	res.status(500).json({ message: "InternalServerError" });
+	const tokenPayload: AccessTkPayload = {
+		role: reqUser.role,
+		userId: reqUser.userId,
+	};
+	return jwt.sign(
+		tokenPayload,
+		process.env.JWT_ACCESS_SECRET,
+		{
+			algorithm: "HS256",
+			expiresIn: "1h",
+		},
+		(err, accessToken) => {
+			if (err) return res.status(500).json({ message: "InternalServerError" });
+
+			return res
+				.cookie("accessToken", accessToken, {
+					expires: new Date(Date.now() + 3600 * 1000),
+					secure: true,
+					httpOnly: true,
+				})
+				.status(200)
+				.send();
+		}
+	);
 };
 
 export default createToken;
