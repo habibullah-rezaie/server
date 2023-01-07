f3573f96046f986f0bed634937e24678ca0242aa
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-01-07 12:39:04 +0300

    Create sessions on login

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,61 +1,113 @@
-import { User } from "@prisma/client";
+import { Role, User } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import prisma from "../../../prisma/prisma";
 
-const login = (req: Request, res: Response, next: NextFunction) => {
+export type RefreshTkPayload = {
+	role: Role;
+	userId: string;
+	sid: string;
+};
+
+export type AccessTkPayload = Omit<RefreshTkPayload, "sid">;
+
+const login = async (req: Request, res: Response, next: NextFunction) => {
 	if (!req.user) {
 		return res.status(500).json({ message: "InternalServerError" });
 	}
 
-	const { email, role, name } = req.user as User;
+	const { role, name, id: userId } = req.user as User;
 
-	if (email && role && process.env.JWT_REFRESH_SECRET) {
+	if (!userId || !role) {
+		console.error("Something has went wrong, email or role is in `req.user`");
+		res.status(500).json({ message: "InternalServerError" });
+	}
+
+	if (!process.env.JWT_REFRESH_SECRET) {
+		console.error("No jwt refresh token secret in env");
+		return res.status(500).json({ message: "InternalServerError" });
+	}
+
+	try {
+		const session = await prisma.session.create({
+			data: { user: { connect: { id: userId } }, loginTime: new Date() },
+			select: { id: true },
+		});
+
+		const refreshTkPayload: RefreshTkPayload = {
+			role,
+			userId,
+			sid: session.id,
+		};
 		return jwt.sign(
-			{ email, role, name },
+			refreshTkPayload,
 			process.env.JWT_REFRESH_SECRET,
 			{
 				algorithm: "HS256",
 				expiresIn: "1d",
 			},
 			(err, refreshToken) => {
-				if (err || !process.env.JWT_ACCESS_SECRET)
-					return res.status(500).json({ message: "InternalServerError" });
+				// Clean the session created previously
+				if (err || !process.env.JWT_ACCESS_SECRET) {
+					console.error(err);
+					return prisma.session
+						.delete({ where: { id: session.id }, select: null })
+						.then(() => {
+							res.status(500).json({ message: "InternalServerError" });
+						});
+				}
 
+				const accessTkPayload: AccessTkPayload = {
+					role,
+					userId,
+				};
 				return jwt.sign(
-					{ email, role, name },
+					accessTkPayload,
 					process.env.JWT_ACCESS_SECRET,
 					{
 						algorithm: "HS256",
 						expiresIn: "1h",
 					},
 					(err, accessToken) => {
-						if (err)
-							return res.status(500).json({ message: "InternalServerError" });
+						if (err) {
+							return prisma.session
+								.delete({
+									where: { id: session.id },
+									select: null,
+								})
+								.then(() => {
+									res.status(500).json({ message: "InternalServerError" });
+								});
+						}
 
+						// session expires in 1 day
+						const sessionExpDate = new Date(Date.now() + 86400 * 1000);
 						return res
 							.cookie("refreshToken", refreshToken, {
-								expires: new Date(Date.now() + 86400 * 1000),
+								expires: sessionExpDate,
 								secure: true,
 								httpOnly: true,
 							})
 							.cookie("accessToken", accessToken, {
-								expires: new Date(Date.now() + 60 * 1000),
+								expires: new Date(Date.now() + 3600 * 1000), // expires in 1 hour
+								secure: true,
+								httpOnly: true,
+							})
+							.cookie("sid", session.id, {
+								expires: sessionExpDate,
 								secure: true,
 								httpOnly: true,
 							})
 							.status(200)
-							.json({ ...req.user, password: undefined });
+							.json({ user: { ...req.user, password: undefined } });
 					}
 				);
 			}
 		);
-	} else if (!process.env.JWT_REFRESH_SECRET) {
-		console.error("No jwt refresh token secret in env");
+	} catch (err) {
+		console.error(err);
 		return res.status(500).json({ message: "InternalServerError" });
 	}
-
-	console.error("Something has went wrong, email or role is in `req.user`");
-	res.status(500).json({ message: "InternalServerError" });
 };
 
 export default login;
